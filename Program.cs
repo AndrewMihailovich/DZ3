d37c430b7c9ca1927fbdb739a41ddb0188018438
Andrew <Andy>
2023-08-04 22:44:43 +0300

    DZ3

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number % 2;
+if (result == 0)
+{
+Console.Write(number + " – чётное");
+}
+else 
+{
+Console.Write(number + " – нечётное");
+}
